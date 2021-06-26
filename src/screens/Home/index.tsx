4f2282e9e251202e2406eba4573ea680753b0277
Kaspary <joaopedrokaspary@hotmail.com>
2021-06-26 19:56:53 -0300

    First commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useCallback } from "react";
+import { 
+    View, 
+    FlatList,
+} from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { styles } from "./styles";
+import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment, AppointmentProps } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+import { Background } from "../../components/Background";
+import { Load } from "../../components/Load";
+import { COLLECTION_APPOINTMENTS } from "../../configs/database";
+
+
+export function Home() {
+    const navigation = useNavigation();
+
+    const [category, setCategory] = useState('');
+    const [loading, setLoading] = useState(true);
+    
+    const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
+
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId)
+    }
+    
+    function handleAppointmentDetails(guildSelected: AppointmentProps) {
+        navigation.navigate('AppointmentDetails', { guildSelected });
+    }
+    
+    function handleAppointmentCreate() {
+        navigation.navigate('AppointmentCreate');
+    }
+
+    async function loadAppointments(){
+        const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+        const response: AppointmentProps[] = storage ? JSON.parse(storage): [];
+
+        if (category){
+            setAppointments(response.filter(item => item.category === category));
+        } else {
+            setAppointments(response);
+        }
+        setLoading(false);
+    }
+
+    useFocusEffect(useCallback(() => {
+        loadAppointments();
+    }, [category]))
+
+    return (
+        <Background>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd onPress={handleAppointmentCreate}/>
+            </View>
+
+            <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+
+            {
+                loading 
+                ? <Load/>
+                : <>
+                    <ListHeader
+                        title="Partidas agendadas"
+                        subtitle={`Total ${appointments.length}`}
+                    />
+
+                    <FlatList
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        style={styles.matches}
+                        showsVerticalScrollIndicator={false}
+                        ItemSeparatorComponent={() => <ListDivider />}
+                        contentContainerStyle={{ paddingBottom: 69 }}
+                        renderItem={({ item }) => (
+                            <Appointment
+                                data={item}
+                                onPress={() => handleAppointmentDetails(item)}
+                            />
+                        )}
+                    />
+                </>
+            }
+        </Background>
+    )
+}
